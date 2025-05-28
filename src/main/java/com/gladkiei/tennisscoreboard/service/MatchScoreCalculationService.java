e34093315165f76b7978ac875bae544b0f74991b
EGladki <e.gladki@gmail.com>
2025-05-28 23:27:32 +0300

    finished MatchScoreCalculationService

--- src/main/java/com/gladkiei/tennisscoreboard/service/MatchScoreCalculationService.java
+++ src/main/java/com/gladkiei/tennisscoreboard/service/MatchScoreCalculationService.java
@@ -10,10 +10,10 @@ import static com.gladkiei.tennisscoreboard.service.OngoingMatchService.*;
 
 public class MatchScoreCalculationService {
     private final static int MAX_SCORE = 40;
-    private final static int MAX_GAMES = 2; // todo fix -> 6
+    private final static int MAX_GAMES = 6; // todo fix -> 6
     private final static int MAX_SETS = 2; // todo fix -> 2
-    private final static int TIEBREAK_SCORE = 1; // todo fix -> 6
-    private final static int DIFFERENCE_IN_SETS_TO_WIN_TIEBREAK = 2;
+    private final static int TIEBREAK_SCORE_TO_WIN = 7;
+    private final static int DIFFERENCE_IN_SCORES_TO_WIN_TIEBREAK = 2;
     private final static int ZERO = 0;
     private final static int ONE_POINT = 1;
     private final static int MORE = 1;
@@ -23,12 +23,34 @@ public class MatchScoreCalculationService {
     private final static int FINAL_ADDING_SCORE = 10;
     private final MatchScoreModelDao matchScoreModelDao = new MatchScoreModelDao();
 
+
     public void updateScore(UUID uuid, Long winnerId) {
-        if (isDeuce(uuid)) {
+        if (isTieBreak(uuid)) {
+            playTiebreak(uuid, winnerId);
+        } else if (isDeuce(uuid)) {
             givePlayerScoreDeuceRules(uuid, winnerId);
         } else {
             givePlayerScore(uuid, winnerId);
         }
+    }
+
+    private void playTiebreak(UUID uuid, Long winnerId) {
+        MatchScoreModel matchScoreModel = matchScoreModelDao.getModel(uuid);
+        PlayerScoreModel winner = getWinner(winnerId, matchScoreModel);
+        PlayerScoreModel loser = getLoser(winnerId, matchScoreModel);
+
+        winner.setPlayerScore(winner.getPlayerScore() + ONE_POINT);
+
+        if (isTiebreakFinished(uuid)) {
+            matchScoreModel.setTieBreak(NOT_TIEBREAK);
+
+
+            updateGame(uuid, winnerId);
+            if (isGameFinished(uuid, winner.getPlayerGame())) {
+                updateSet(uuid, winnerId);
+            }
+        }
+
     }
 
     private boolean isDeuce(UUID uuid) {
@@ -58,14 +80,13 @@ public class MatchScoreCalculationService {
         PlayerScoreModel winner = getWinner(winnerId, matchScoreModel);
         PlayerScoreModel loser = getLoser(winnerId, matchScoreModel);
 
-
         winner.setPlayerScore(winner.getPlayerScore() + MORE);
         loser.setPlayerScore(loser.getPlayerScore() + LESS);
 
         if (winner.getPlayerScore() > MORE) {
             matchScoreModel.setDeuce(NOT_DEUCE);
             updateGame(uuid, winnerId);
-            if (isGameFinished(winner.getPlayerGame())) {
+            if (isGameFinished(uuid, winner.getPlayerGame())) {
                 updateSet(uuid, winnerId);
             }
         }
@@ -75,18 +96,18 @@ public class MatchScoreCalculationService {
         MatchScoreModel matchScoreModel = matchScoreModelDao.getModel(uuid);
         PlayerScoreModel winner = getWinner(winnerId, matchScoreModel);
 
-        int score = calculateAddingScore(winner.getPlayerScore());
+        int score = chooseAddingScore(winner.getPlayerScore());
         winner.setPlayerScore(winner.getPlayerScore() + score);
 
         if (isMatchFinished(winner.getPlayerScore())) {
             updateGame(uuid, winnerId);
-            if (isGameFinished(winner.getPlayerGame())) {
+            if (isGameFinished(uuid, winner.getPlayerGame())) {
                 updateSet(uuid, winnerId);
             }
         }
     }
 
-    private int calculateAddingScore(int score) {
+    private int chooseAddingScore(int score) {
         switch (score) {
             case ZERO -> {
                 return FIRST_ADDING_SCORE;
@@ -107,26 +128,23 @@ public class MatchScoreCalculationService {
 
         winner.setPlayerScore(START_SCORE);
         winner.setPlayerGame(winner.getPlayerGame() + ONE_POINT);
-
         loser.setPlayerScore(START_SCORE);
     }
 
     public void updateSet(UUID uuid, Long winnerId) {
         MatchScoreModel match = MatchScoreModelDao.getInstance().getModel(uuid);
-
         PlayerScoreModel winner = getWinner(winnerId, match);
         PlayerScoreModel loser = getLoser(winnerId, match);
 
-        winner.setPlayerScore(START_SCORE);
-        winner.setPlayerGame(START_GAME);
         winner.setPlayerSet(winner.getPlayerSet() + ONE_POINT);
 
-        isTieBreak(uuid); // todo
-
-        if (isSetFinished(uuid, winner.getPlayerSet())) {
+        if (!hasNeededExtraMatch(uuid) && isSetFinished(uuid, winner.getPlayerSet())) {
             winner.setWinner(WINNER);
             match.setState(COMPLETED);
         }
+
+        winner.setPlayerScore(START_SCORE);
+        winner.setPlayerGame(START_GAME);
         loser.setPlayerScore(START_SCORE);
         loser.setPlayerGame(START_GAME);
     }
@@ -136,14 +154,22 @@ public class MatchScoreCalculationService {
         if (matchScoreModel.isTieBreak()) {
             return true;
         }
-        int player1Set = matchScoreModel.getPlayer1ScoreModel().getPlayerSet();
-        int player2Set = matchScoreModel.getPlayer2ScoreModel().getPlayerSet();
-        if (player1Set == MAX_GAMES && player2Set == MAX_GAMES) {
+        int player1Game = matchScoreModel.getPlayer1ScoreModel().getPlayerGame();
+        int player2Game = matchScoreModel.getPlayer2ScoreModel().getPlayerGame();
+        if (player1Game == MAX_GAMES && player2Game == MAX_GAMES) {
             matchScoreModel.setTieBreak(TIEBREAK);
+            resetScore(uuid);
             return true;
         } else {
             return false;
         }
+    }
+
+    private boolean hasNeededExtraMatch(UUID uuid) {
+        MatchScoreModel matchScoreModel = matchScoreModelDao.getModel(uuid);
+        int player1Game = matchScoreModel.getPlayer1ScoreModel().getPlayerGame();
+        int player2Game = matchScoreModel.getPlayer2ScoreModel().getPlayerGame();
+        return ((player1Game == 5 && player2Game == 6) || (player1Game == 6 && player2Game == 5));
     }
 
     private PlayerScoreModel getWinner(Long winnerId, MatchScoreModel matchScoreModel) {
@@ -166,18 +192,21 @@ public class MatchScoreCalculationService {
         return score > MAX_SCORE;
     }
 
-    private boolean isGameFinished(int games) {
-        return games >= MAX_GAMES;
+    private boolean isGameFinished(UUID uuid, int games) {
+        return games >= MAX_GAMES && !hasNeededExtraMatch(uuid) && !isTieBreak(uuid);
     }
 
     private boolean isSetFinished(UUID uuid, int sets) {
-        MatchScoreModel matchScoreModel = matchScoreModelDao.getModel(uuid);
-        if (matchScoreModel.isTieBreak()) {
-            int player1Set = matchScoreModel.getPlayer1ScoreModel().getPlayerSet();
-            int player2Set = matchScoreModel.getPlayer2ScoreModel().getPlayerSet();
-            return ((player2Set - player1Set) == DIFFERENCE_IN_SETS_TO_WIN_TIEBREAK || (player1Set - player2Set) == DIFFERENCE_IN_SETS_TO_WIN_TIEBREAK);
-        }
         return sets == MAX_SETS;
+    }
+
+    private boolean isTiebreakFinished(UUID uuid) {
+        MatchScoreModel matchScoreModel = matchScoreModelDao.getModel(uuid);
+        int player1Score = matchScoreModel.getPlayer1ScoreModel().getPlayerScore();
+        int player2Score = matchScoreModel.getPlayer2ScoreModel().getPlayerScore();
+
+        return (player1Score >= TIEBREAK_SCORE_TO_WIN && (player1Score - player2Score) >= DIFFERENCE_IN_SCORES_TO_WIN_TIEBREAK) ||
+               (player2Score >= TIEBREAK_SCORE_TO_WIN && (player2Score - player1Score) >= DIFFERENCE_IN_SCORES_TO_WIN_TIEBREAK);
     }
 
 }
